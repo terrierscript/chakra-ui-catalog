69805500a01e6154878e0bccd7542f1fbc5ad74a
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-18 22:27:23 +0900

    add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,31 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import React from "react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { Box, ChakraProvider, Divider, HStack } from "@chakra-ui/react"
+import React, { useMemo } from "react"
 
-function MyApp({ Component, pageProps }) {
+const Footer = () => {
+  const url = useMemo(() => {
+    const pathname = window.location.pathname
+    const initialPath = encodeURIComponent(pathname)
+    const module = encodeURIComponent(`src/pages/${pathname}.tsx`)
+    return `https://codesandbox.io/embed/github/terrierscript/chakra-ui-sandbox/tree/main/?fontsize=14&initialpath=${initialPath}&module=${module}&theme=dark`
+
+  }, [])
+  return <Box p={4} fontSize={"xs"} color={"gray.400"}>
+    <Divider />
+    <HStack p={4}>
+      <Box userSelect="all">
+        @[codesandbox]({url})
+      </Box>
+      <a href={url} target="_blank">
+        <ExternalLinkIcon />
+      </a>
+    </HStack>
+  </Box >
+}
+function MyApp({ Component, pageProps }: any) {
   return <ChakraProvider>
-   <Component {...pageProps} />
+    <Component {...pageProps} />
+    <Footer />
   </ChakraProvider>
 }
 
