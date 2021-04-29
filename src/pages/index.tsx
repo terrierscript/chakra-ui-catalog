1964ed0ca99c0b06ef74d1a12fcda8dc07f0a8c8
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-29 23:41:41 +0900

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
 import Head from 'next/head'
 import React from 'react'
+import NextLink from "next/link"
+// @ts-ignore
+import recursiveReaddir from "recursive-readdir"
+import { Box, Stack } from '@chakra-ui/react'
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const files = await recursiveReaddir("./src/pages")
+  const pathnames = files
+    .map((file: string) => {
+      const pathname = file
+        .replace("src/pages/", "") // remove basedir
+        .replace(/.[jt]sx?$/, "") // remove exteension
+      return pathname
+    })
+    .filter((file: string) => !/^_.+/.test(file))
+    .filter((file: string) => file !== "index")
+  return {
+    props: { pathnames }
+  }
+}
+
+export default function Home({ pathnames }: { pathnames: string[] }) {
+
   return (
-    <div>
-      <div>hello ____</div>
-    </div>
+    <Box p={10}>
+      <Stack>
+        {pathnames.map(p => {
+          return <NextLink href={p}>{p}</NextLink>
+        })}
+      </Stack>
+    </Box>
   )
 }
 
